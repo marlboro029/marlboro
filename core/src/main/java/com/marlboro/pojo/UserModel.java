40fab4f0b9b547e8b5a9b7ebd8d0719b0a966b9b
marlboro <marlboro027@foxmail.com>
2016-05-13 00:00:31 +0800

    git init

---
+++ core/src/main/java/com/marlboro/pojo/UserModel.java
@@ -0,0 +1,27 @@
+package com.marlboro.pojo;
+
+/**
+ * Marlboro
+ * 2016/5/12
+ */
+public class UserModel {
+    private String name;
+
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+}
